de0fa5c9cc307af2ef9e00966d3b5928f0bd7672
Leandro Angiolini <angiolini.leandro@gmail.com>
2016-04-25 03:52:31 -0300

    Menos clases mas bloques

--- src/matchers.rb
+++ src/matchers.rb
@@ -1,101 +1,41 @@
-class Matchers
-  attr_accessor :matcher, :collection
+class Matcher
+  attr_accessor :objAMatchear, :bloqueDeMatcheo
 
-  def initialize(object)
-    self.matcher = object
+  def initialize(objAMatchear,&bloque)
+    self.objAMatchear = objAMatchear
+    self.bloqueDeMatcheo = bloque
   end
 
-  def call(object)
+  def call(objetoAComparar)
+    self.bloqueDeMatcheo.call(self.objAMatchear,objetoAComparar)
   end
 
-  def armarMatchCollection(*matchers,&bloque)
-    self.collection = MatchCollection.new &bloque
-    self.collection.add([self])
-    self.collection.add(matchers)
-    self.collection
-  end
   def and(*matchers)
-    armarMatchCollection(*matchers){|coll,obj| coll.all? {|matcher| matcher.call(obj)}}
+    arrayDeMatchers = [self] + matchers
+    Combinators.new(arrayDeMatchers) {|listaDeMatchers,objectoAComparar|
+      listaDeMatchers.all? {|matcher|
+        matcher.call(objectoAComparar)
+      }
+    }
   end
 
   def or(*matchers)
-    armarMatchCollection(*matchers){|coll,obj| coll.any? {|matcher| matcher.call(obj)}}
-  end
-
-  def not
-    NotMatcher.new self
-  end
-end
-
-##################################################################
-
-class VariableMatcher < Matchers
-  def call(object)
-    self.matcher == object
-  end
-end
-
-##################################################################
-
-class TypeMatcher < Matchers
-  def call(object)
-    object.class.ancestors.to_a.include?(self.matcher)
-  end
-end
-
-##################################################################
-
-class ListMatcher < Matchers
-  def initialize(object, sizeMatch = true)
-    self.matcher = object
-  end
-
-  def call(object)
-
-  end
-end
-
-##################################################################
-
-class DuckMatcher < Matchers
-  def initialize(*methods)
-    self.matcher = *methods
-  end
-  def call(object)
-      self.matcher.to_a.all? {
-        |x|  object.methods.to_a.include? x
+    arrayDeMatchers = [self] + matchers
+    Combinators.new(arrayDeMatchers) {|listaDeMatchers,objectoAComparar|
+      listaDeMatchers.any? {|matcher|
+        matcher.call(objectoAComparar)
       }
-  end
-end
-
-##################################################################
-
-class MatchCollection < Matchers
-  attr_accessor :bloque
-  def initialize(&bloq)
-    self.matcher = []
-    self.bloque = bloq
-  end
-
-  def add(matchers)
-    matchers.to_a.each {|matcher| self.matcher << matcher}
-  end
-
-  def call(object)
-    bloque.call(self.matcher,object)
-  end
-end
-
-class NotMatcher < Matchers
-  def initialize(matcher)
-    self.matcher = matcher
-  end
-
-  def call(object)
-    !self.matcher.call object
+    }
   end
 
   def not
-    self
+    Matcher.new(self) {|matcher,objectoAComparar|
+      !matcher.call(objectoAComparar)
+    }
   end
+end
+
+################################################################
+
+class Combinators < Matcher
 end
--- src/pattern_matching.rb
+++ src/pattern_matching.rb
@@ -8,23 +8,47 @@ class Symbol
 end
 
 ############################################################################
-
+#self.instance_eval do
 class Object
   def val(param)
     if(param.class == Symbol)
       return param
     end
-    VariableMatcher.new param
+    Matcher.new(param) {|x,y| x == y}
   end
 
   def type(clase)
     unless clase.class == Class || clase.class == Module
       raise 'El parametro de type debe ser una Clase o Modulo'
     end
-    TypeMatcher.new clase
+    Matcher.new(clase) {|x,y| y.class.ancestors.to_a.include?(x) }
   end
 
   def duck(*methods)
-    DuckMatcher.new *methods
+    Matcher.new(methods) {|x,y| x.all? {|method| y.methods.to_a.include?(method)}}
+  end
+
+  def list(array, cond = true)
+    raise 'Debe ser un array'  unless type(Array).call(array)
+    Matcher.new(array) {
+      |x,y|
+
+      if x.size <= y.size && (!cond || x.size == y.size)
+        respuestas = []
+        x.size.times {|time|
+          if type(Matcher).call(x[time]) || x[time].class == Symbol #cambiar por type
+            respuestas << x[time].call(y[time])
+            puts 'era Matcher o Symbol' + x[time].to_s + y[time].to_s
+          else
+            respuestas << val(x[time]).call(y[time])
+            puts 'era una valor x: ' + x[time].to_s + ' y: ' + y[time].to_s
+          end
+
+        }
+        respuestas.all? {|respuesta| respuesta}
+      else
+        false
+      end
+    }
   end
 end
